af093846ab3465c78503198e4274c167d6b027e3
You95 <youness.95@live.com>
2018-01-02 15:22:38 +0100

    Finale

---
+++ src/SchoolBundle/Controller/InformationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SchoolBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use SchoolBundle\Entity\TravailRealise;
+use UserBundle\Entity\Eleve;
+use SchoolBundle\Entity\Classe;
+use SchoolBundle\Form\TravailRealiseType;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Information controller.
+ *
+ * @Route("/information")
+ */
+class InformationController extends Controller
+{
+    /**
+     * @Route("/eleve/{id}", name="eleve_information")
+     * @Method({"GET", "POST"})
+     */
+    public function showEleveAction(Classe $classe,Request $request)
+    { 
+        $provider = $this->container->get('fos_message.provider');
+        $inbox = $provider->getInboxThreads();
+        $sentbox = $provider->getSentThreads();
+        $nb=$provider->getNbUnreadMessages();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $data['travailfait']=$em->getRepository('SchoolBundle:TravailRealise')->findBy(array('classe'=>$classe),array('date' => 'desc'));
+        $data['travailafaire']=$em->getRepository('SchoolBundle:TravailaFaire')->findBy(array('classe'=>$classe),array('datelimite' => 'desc'));
+               
+
+        return $this->render('information/eleve.html.twig',array("data"=>$data,'newMessages'=>$nb,'inbox'=>$inbox,"sentbox"=>$sentbox));  
+   
+    }
+
+
+}
